4107e2b77887caa021898d23edcade6af5067e75
subway90 <nguyenhieu3105@gmail.com>
2025-07-28 21:06:25 +0700

    update

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once 'autoload.php';
 # [DEPLOY]
 if(get_action_uri(0) == 'deploy-src') controller('user','deploy-src');
 
+# [BOOL ACTIVE]
+view_error(404);
 
 # [ACTION]
 if ($action = get_action_uri(0)) controller('user',$action);
